0cf9d5303d9683bf77457f7cbc5905aa7ad3e53b
Ida Nyman <ida.nyman@klarna.com>
2022-02-15 17:58:48 +0100

    Add Telescope configuration

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,53 @@
 local map = require('keybinds').map
 
+local center_dropdown = {
+  path_display = { 'smart' },
+  theme = 'dropdown',
+  layout_config = {
+    center = {
+      width = 120,
+    },
+  },
+}
+
+local cursor = {
+  layout_strategy = 'cursor',
+  sorting_strategy = 'ascending',
+  layout_config = {
+    cursor = {
+      width = 60,
+      height = 8,
+    }
+  }
+}
+
+require('telescope').setup({
+  pickers = {
+    lsp_references = center_dropdown,
+    lsp_definitions = vim.tbl_extend('error', center_dropdown, {
+      jump_type = 'never',
+    }),
+    lsp_code_actions = cursor,
+    lsp_range_code_actions = cursor,
+    lsp_document_diagnostics = center_dropdown,
+    lsp_document_symbols = center_dropdown,
+    lsp_dynamic_workspace_symbols = center_dropdown,
+    live_grep = center_dropdown,
+    buffers = center_dropdown,
+    registers = center_dropdown,
+    find_files = center_dropdown,
+    git_files = center_dropdown
+  },
+  defaults = {
+    path_display = {
+      "shorten",
+      "absolute",
+    },
+  }
+})
+
 -- Keybinds
-map {'n', '<leader>ff', ':Telescope find_files<cr>'}
+map {'n', '<leader>ff', ':Telescope git_files<cr>'}
 map {'n', '<leader>fb', ':Telescope buffers<cr>'}
 map {'n', '<leader>fg', ':Telescope live_grep<cr>'}
 
