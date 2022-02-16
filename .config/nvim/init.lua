6e719081c2b4cca4dacde45dcf1260575a89d488
Ida Nyman <ida.nyman@klarna.com>
2022-02-16 15:22:19 +0100

    Enable mouse, right split and closing buffer only

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,18 +1,21 @@
 require('plugins')
-require('bufferline').setup{}
 
 local map = require('keybinds').map
 
+-- General configuration
 vim.g.mapleader = ' '
+vim.opt.mouse = 'a'
+vim.opt.splitright = true
+vim.cmd('colorscheme nord')
+vim.cmd('set shell=/bin/zsh')
+vim.cmd('language en_US')
+vim.wo.number = true
 
 -- Neovide configuration
 vim.opt.guifont = 'FiraCode Nerd Font:h13'
 vim.opt.termguicolors = true
 
-vim.cmd('colorscheme nord')
-vim.cmd('set shell=/bin/zsh')
-vim.cmd('language en_US')
-
--- Show line numbers
-vim.wo.number = true
+-- General keybinds
+-- Close buffer without closing window
+map {'n', '<leader>q', ':bp<bar>sp<bar>bn<bar>bd<cr>'}
 
